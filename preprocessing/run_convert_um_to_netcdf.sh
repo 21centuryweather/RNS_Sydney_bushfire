bf25651af48361d7f24172b3890354dc9bba45b5
abbysmyth <asmy0002@student.monash.edu>
2025-10-07 13:04:41 +1100

    storage project change

--- preprocessing/run_convert_um_to_netcdf.sh
+++ preprocessing/run_convert_um_to_netcdf.sh
@@ -3,10 +3,10 @@
 #PBS -l ncpus=12
 #PBS -l mem=48GB
 #PBS -l walltime=10:00:00
-#PBS -l storage=gdata/xp65+scratch/ce10+gdata/fy29+scratch/public
+#PBS -l storage=gdata/xp65+gdata/fy29+scratch/ng72+gdata/ng72
 #PBS -l wd
 #PBS -l jobfs=10GB
-#PBS -P fy29
+#PBS -P ng72
 
 set -eu
 module purge
@@ -14,4 +14,4 @@ module use /g/data/xp65/public/modules
 module load conda/analysis3
 # module load dask-optimiser
 
-python $HOME/git/RNS_Sydney_bushfire/convert_um_to_netcdf.py
+python $HOME/Honours/github_honours/RNS_Sydney_bushfire/convert_um_to_netcdf.py
